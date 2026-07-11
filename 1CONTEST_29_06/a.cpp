d5481fabfc4d6e8da3da80605a3b3dd322049d02
Fedor <144064673+FrudenFireKing@users.noreply.github.com>
2026-07-11 08:45:16 +0300

    Create a.cpp

---
+++ 1CONTEST_29_06/a.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int count;
+
+    cin >> count;
+
+    for (int i = 0; i < count; i++) {
+        int x;
+        cin >> x;
+        cout << x << "\n";
+    }
+    return 0;
+}
